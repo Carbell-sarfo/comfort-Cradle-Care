9a0b935324ab911641836ecabdfbe36540722e6f
Carbell Sarfo <carbellsarfo@gmail.com>
2023-05-16 23:33:47 +0000

    ready to upload

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/js");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
